12830819295190e03a09b5fc425d1a22ac5e8181
maksudulrochee <maksudul.rochee21@gmail.com>
2021-01-04 22:59:56 -0500

    version 2

--- src/test/java/scripts/signupTest.java
+++ src/test/java/scripts/signupTest.java
@@ -3,7 +3,8 @@ package scripts;
 public class signupTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("this is signup page");
 
 	}
 
---
+++ src/test/java/scripts/testingGit.java
@@ -0,0 +1,12 @@
+package scripts;
+
+public class testingGit {
+
+	public static void main(String[] args) {
+
+
+		System.out.println("this it for git");
+
+	}
+
+}
